8578a1850064094f01dfba9f2f138976ae7deaf9
kAI-ndesmarais <43179822+kAI-ndesmarais@users.noreply.github.com>
2018-09-25 11:27:18 -0400

    added network class

---
+++ TetrisClone/src/main/java/Network.java
@@ -0,0 +1,15 @@
+//This class houses the layers of the neural network
+public class Network {
+
+    private int INPUT_NUMBER = 208;
+    private int NODES = 1000;
+    /*(MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .layer(0, new DenseLayer.Builder()
+                    .nIn(INPUT_NUMBER)
+                    .nOut(NODES)
+                    .weightInit(WeightInit.RELU)
+                        .activation(Activation.SIGMOID)
+                        .build()
+            )
+    */
+}
