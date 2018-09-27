31cc7d84f8032069d34080c285054e1ea919137c
Forest Dils <fdils@khsnet.msad71.net>
2018-09-27 12:18:16 -0400

    SumActFunction
    Beginning of Summing/Activation Function

---
+++ TetrisClone/src/main/java/SumActFunction.java
@@ -0,0 +1,28 @@
+public class SumActFunction {
+
+    private int well[][];
+   private boolean value[][];
+   private int i, j, i2, j2;
+
+    SumActFunction(int well[][], boolean value[][], int i, int j, int i2, int j2) {
+        this.well = well;
+        this.value = value;
+        this.i = i;
+        this.j = j;
+        this.i2 = i2;
+        this.j2 = j2;
+
+        for(i = 0; i <)
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
