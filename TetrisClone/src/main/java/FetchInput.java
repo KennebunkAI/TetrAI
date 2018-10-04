70ee46fa6f19a821e40f6653202aab7d7a7492a7
Forrest Dils <forrest.l.dils@gmail.com>
2018-10-04 19:10:44 -0400

    removed random spacing
    oops

--- TetrisClone/src/main/java/FetchInput.java
+++ TetrisClone/src/main/java/FetchInput.java
@@ -4,9 +4,7 @@ class FetchInput {
     private static boolean value[][] = new boolean[12][24];
 
     FetchInput() {
-
     }
-
         public static boolean[][] colorOuput(Color[][] well) {
             for (int i = 0; i < 12; i++) {
                 for (int j = 0; j < 24; j++) {
@@ -19,9 +17,4 @@ class FetchInput {
             }
             return value;
         }
-
-
-
-
-
 }
